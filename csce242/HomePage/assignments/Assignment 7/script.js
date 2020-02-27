57c825a26c6c39d53aeb4f4ee2162bef5389ec76
milz L <13milesj@gmail.com>
2020-02-27 10:23:30 -0500

    adding assignment 7

--- csce242/HomePage/assignments/Assignment 7/script.js
+++ csce242/HomePage/assignments/Assignment 7/script.js
@@ -1,19 +1,20 @@
 let margin = 0;
+let grow = 0;
 function Walking() {
     margin += 10;
-
-    if (document.getElementById("picture").src = "images/walking-one.png") {
-      
-        document.getElementById("picture").src = "images/walking-two.png";
-
-    } else if (document.getElementById("picture").src = "images/walking-two.png") {
-        
-        document.getElementById("picture").src = "images/walking-one.png";
-        
-    }
-
     document.getElementById("picture").style.setProperty('--dim', margin + "px");
+}
+
+function fillGradient() {
+    let number = document.getElementById("txt-funds").value * 100;
+    grow = number/10000
+    document.getElementById("grad").style.setProperty('--blank', grow + "%");
+
+
 }
 
 const btnWalk = document.getElementById("picture");
 btnWalk.onclick = Walking;
+
+const btnDisplay = document.getElementById("display");
+btnDisplay.onclick = fillGradient;
