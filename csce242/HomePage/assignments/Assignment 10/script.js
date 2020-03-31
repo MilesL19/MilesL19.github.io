278405c70bb3f3abba1f8ab1b59d7133a05b2aee
milz L <13milesj@gmail.com>
2020-03-30 20:27:15 -0400

    starting assignment 10

---
+++ csce242/HomePage/assignments/Assignment 10/script.js
@@ -0,0 +1,55 @@
+async function showMovies() {
+    let response = await fetch(`https://milesl19.github.io/csce242/HomePage/json/movies.json`);
+    let moviesJSON = await response.json();
+    let movieDiv = document.createElement("movie-section");
+
+    //Looping to get movies
+    for (i in moviesJSON) {
+        let movie = moviesJSON[i];
+        movieDiv.append(getMovieItem(movie));
+    }
+}
+
+function getMovieItem(movie) {
+    let movieSection = document.createElement("section");
+
+    let divPic = document.createElement("div");
+    let image = document.createElement("img");
+    image.src = `images/${movies.img}`;
+    divPic.append(image);
+    movieSection.append(divPic);
+
+    let h2Elem = document.createElement("h2");
+    h2Elem.innerText = `${movie.title}`;
+    movieSection.append(h2Elem);
+
+    let ulElem = document.createElement("ul");
+    movieSection.append(ulElem);
+
+    let liDirector = document.createElement("li");
+    liDirector.innerText = `Director: ${movie.director}`;
+    ulElem.append(liBrand);
+
+    let liActors = document.createElement("li");
+    liActors.innerText = `Actors: ${movie.actors}`;
+    ulElem.append(liActor);
+
+    let liYear = document.createElement("li");
+    liYear.innerText = `Years: ${movies.year}`;
+    ulElem.append(liYear);
+
+    let liGenre = document.createElement("li");
+    liGenre.innerText = `Genres: ${movies.genres}`;
+    ulElem.append(liGenre);
+
+    let liDescription = document.createElement("li");
+    liDescription.innerText = `${movies.description}`;
+    ulElem.append(liDescription);
+
+    return movieSection;
+
+
+
+    window.onload = function() {
+        this.showMovies();
+    }}
--- csce242/HomePage/assignments/Participation Assignment 1/script.js
+++ csce242/HomePage/assignments/Participation Assignment 1/script.js
@@ -3,15 +3,40 @@ async function Brewery() {
     let response = await fetch(`https://milesl19.github.io/csce242/HomePage/json/breweries.json`);
     let breweryJSON = await response.json();
     let breweryDiv = document.getElementById("brewery");
+    let count = 0;
 
     for (i in breweryJSON) {
         brewery = breweryJSON[i];
-        breweryDiv.append(getBreweryInfo(brewery));
     }
+    
+    //Creating the container for the info to be displayed in
+    let container = document.createElement("div");
+
+    //Creating a brewery object to hold current brewery and image to place info in 
+    let currBrew = document.createElement("section");
+    container.classList.add("container");
+    let image = document.createElement("img");
+    image.src = `images/text-box.png`;
+    image.style.height = `400px`;
+    image.style.width = `500px`;
+    image.classList.add("container");
+    container.append(image);
+
+    
+    container.append(currBrew);
+    currBrew.classList.add("container");
+    currBrew.classList.add("center");
+    currBrew.append(getBreweryInfo(brewery));
+    
+    breweryDiv.append(container);
+    /*while (count != brewery.length) {
+        breweryDiv.append(brewery[count]);
+    }*/
+
 }
 
 function getBreweryInfo(brewery) {
-    //Getting information and appending it to image
+    //Getting information 
     let brewSect = document.createElement("section");
 
     let ulElem = document.createElement("ul");
@@ -19,33 +44,43 @@ function getBreweryInfo(brewery) {
     brewSect.append(ulElem);
 
     let liName = document.createElement("li");
-    liName.innertext = `Name: ${brewery.name}`;
+    liName.innerText = `Name: ${brewery.name}`;
     ulElem.append(liName);
 
     let liType = document.createElement("li");
-    liType.innertext = `This is a ${brewery.brewery_type}`;
+    liType.innerText = `This is a ${brewery.brewery_type}`;
     ulElem.append(liType);
 
     let liAddress = document.createElement("li")
-    liAddress.innertext = 
+    liAddress.innerText = 
     `Address: ${brewery.city}, ${brewery.state} 
     ${brewery.postal_code} ${brewery.country}`;
     ulElem.append(liAddress);
 
-    let liUrl = document.createElement("li");
     let aWeb = document.createElement("a");
-    aWeb.href = `${brewery.website_url}`;
-    liUrl.append(aWeb);
-    liUrl.innertext = "Click me for website ;)";
-    ulElem.append(liUrl);
+    aWeb.href = brewery.website_url;
+    aWeb.innerText = "Click me for the website ;)";   
+    ulElem.append(aWeb);
 
     return brewSect;
 }
 
-function appendDiv(div, brewery) {
-    div.append(getBreweryInfo(brewery));
-}
+/*function appendDiv(div, brewery) {
+    let container = document.createElement("div");
+    let overlay = document.createElement("div");
+
+    overlay.classList.add("overlay");
+    container.classList.add("container");
+    let image = document.createElement("img");
+    image.src = `images/text-box.png`;
+    container.append(image);
+    overlay.append(getBreweryInfo(brewery));
+    container.append(overlay);
+    div.append(container);
+
+}*/
 
 window.onload = function() {
     this.Brewery();
+
 }
